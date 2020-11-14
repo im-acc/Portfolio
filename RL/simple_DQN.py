b57903b7b8d5dc85c0c7b3500dbe6f14d353681c
Imad Mechmachi <52804372+Gucciimad@users.noreply.github.com>
2020-11-14 15:34:32 -0500

    Update simple_DQN.py

--- RL/simple_DQN.py
+++ RL/simple_DQN.py
@@ -10,8 +10,7 @@ class q_net:
         space and discrete action space. e-greedy exploration. Fully connected
         network, 2 relu hidden layers.
     """
-    
-    
+   
     def __init__(self, n_states_dim, n_action):
         self.n_states_dim = n_states_dim
         self.n_action = n_action
@@ -23,7 +22,6 @@ class q_net:
             tf.keras.layers.Dense(n_action)
         ])
         self.model.compile(optimizer='adam', loss='mse')
-        
         self.loss = []
         self.steps = []
     
@@ -31,7 +29,6 @@ class q_net:
     def predict(self, state):
         return np.argmax(self.model(np.array([state])))
 
-    
     def learn_from_memory(self, mini_batch_size=30, Y=0.95):
         '''
             Experience replay : trains for 1 epoch on the entire
@@ -39,14 +36,12 @@ class q_net:
         '''
         
         batch = self.memory_queue
-            
         states = batch[:, :self.n_states_dim]
         actions = batch[:, self.n_states_dim]
         new_states = batch[:, self.n_states_dim+1:-1]
         rewards = batch[:, -1]
             
         target = self.model.predict(states)
-
         q_target = rewards + Y * np.max(self.model(new_states), axis=1)
         for i in range(len(batch)):
                 target[i,int(actions[i])] = q_target[i]
@@ -54,30 +49,24 @@ class q_net:
         history = self.model.fit(states, target, batch_size=mini_batch_size, epochs=1, verbose=0)
         self.loss = self.loss + history.history['loss']
 
-    
     def train(self, env, n_episode=100, mini_batch_size=30, eps=0.20, Y=0.95, memory_queue_length=10_000):
         '''
             Training loop : trains the network on the provided OpenAI gym environement.
         '''
         for n in range(n_episode):
             obs = env.reset()
-            
             for t in range(200):
-                
                 if np.random.rand() < eps:
                     action = np.random.randint(self.n_action)
                 else:
                     action = self.predict(obs)
-                
                 new_obs, R, done, info = env.step(action)
                 if done:
                     R -= 15
                     self.steps.append(t)
                 self.memory_queue = np.vstack((self.memory_queue, np.concatenate( ( obs, ar([action]), new_obs, ar([R]) ) )))
-                
                 if done:
                     break
-                
                 obs = new_obs
             
             self.learn_from_memory(mini_batch_size=mini_batch_size, Y=0.95)
