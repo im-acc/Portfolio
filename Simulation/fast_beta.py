4e14491aa5aaefad9672db515a631d36c22b0a5f
Imad Mechmachi <52804372+Gucciimad@users.noreply.github.com>
2020-11-13 20:02:55 -0500

    Add files via upload

---
+++ Simulation/fast_beta.py
@@ -0,0 +1,48 @@
+import torch as to
+
+# fast beta sampler for alpha, beta >= 1, and not bigger than ~ 2-3
+# in this interval, 3-4 x faster than pytroch's function
+
+""" A fast beta sampling algorithm for a,b >= 1 and ~< 3
+
+    According to benchmarks, the algorithm performs on
+    average faster by a factor 1.3x for numpy.random.beta
+    and ~3 x torch.distribution.beta.Beta 
+    
+"""
+
+def sample(alpha, beta, n=1):
+    """
+        Generating n samples ~ Beta(alpha, beta) by the rejection methode
+    """
+    
+    # Computing mode
+    if alpha==1 or beta==1: 
+        mode = max(alpha, beta)
+    else:
+        mode = 1/B(alpha, beta)*((alpha-1)/(alpha+beta-2))**(alpha-1) * ((beta-1)/(alpha+beta-2))**(beta-1)
+
+    # Computing normalizing cst B(alpha,beta)
+    log_gammas = to.lgamma(to.tensor([alpha, beta, alpha + beta], dtype=float))
+    B_cst = to.exp(log_gammas[0] + log_gammas[1] - log_gammas[2])
+
+    return _beta_sample(alpha, beta, mode, B_cst, n)
+
+
+def _beta_sample(alpha, beta, mode, B_cst, n=1):
+    
+    m = int(n*mode)
+    u = to.rand(m)
+    x = to.rand(m)
+    
+    x = x[u < _beta_pdf(alpha, beta, B_cst, x)/mode]
+    
+    nb_left = n-len(x)
+    
+    if nb_left <= 0:
+        return x[:n]
+    else :
+        return to.cat((x, _beta_sample(alpha, beta, mode, B_cst, nb_left)))
+
+def _beta_pdf(alpha, beta, B_cst, x):
+    return to.pow(x, alpha-1)*to.pow(1-x, beta-1)/B_cst
